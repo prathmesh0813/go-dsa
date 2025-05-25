a5469b41bfd499c221014265c2d66012c444607f
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-05-25 12:31:00 +0530

    Time: 1 ms (80.25%) | Memory: 3.8 MB (85.33%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.go
@@ -0,0 +1,14 @@
+func fib(n int) int {
+    if n == 0{
+        return 0
+    }
+    if n == 1{
+        return 1
+    }
+
+    n1,n2 := 0,1
+    for i:=2;i<=n;i++{
+        n1,n2 = n2,n1+n2
+    }
+    return  n2
+}

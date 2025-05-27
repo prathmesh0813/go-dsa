b87e3dad413b1dd6930b727a0acb4102c535c46f
Prathmesh Gonjare <prathmesh.gon@gmail.com>
2025-05-27 20:23:19 +0530

    Printing Subsequences whose sum is K using backtracking

---
+++ Recursion/backtracking02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func printSum(ind int, ds []int, s int, sum int, arr []int, n int) {
+	if ind == n {
+		if s == sum {
+			for _, val := range ds {
+				fmt.Print(val," ")
+			}
+			fmt.Println()
+		}
+		return
+	}
+
+	//Pick the element
+	ds = append(ds, arr[ind])
+	
+	printSum(ind+1,ds,s+arr[ind],sum,arr,n)
+
+	//Backtracking
+	ds = ds[:len(ds)-1]
+	printSum(ind+1,ds,s,sum,arr,n)
+}
--- Recursion/main.go
+++ Recursion/main.go
@@ -1,6 +1,16 @@
 package main
 
 func main() {
-	n := 4
-	print1toN(1,n)
+	//Backtracking01 
+	// n := 4
+	// print1toN(1,n)
+
+	//Backtracking02
+
+	arr := [] int {1,2,1}
+	n := len(arr)
+	sum := 2
+	ds := []int {}
+
+	printSum(0,ds,0,sum,arr,n)
 }

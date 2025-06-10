c61d26964cdc1e71174e002ed3390c7a97cc11e0
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-06-10 22:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (6.33%) - LeetSync

--- 189-rotate-array/rotate-array.go
+++ 189-rotate-array/rotate-array.go
@@ -7,9 +7,9 @@ func rotate(nums []int, k int)  {
 
     k = k%n
 
-    reverse(nums,0,n-1)
-    reverse(nums,0,k-1)
-    reverse(nums,k,n-1)
+    reverse(nums,0,n-1)//reverse the entire array
+    reverse(nums,0,k-1)//reverse the first k elements
+    reverse(nums,k,n-1)//reverse the remaining element
 }
 
 func reverse (nums []int, start,end int){

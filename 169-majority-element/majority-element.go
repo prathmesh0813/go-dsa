1d647f10942e6b4d2941d594fc5de78bf6935f00
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-06-16 21:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (57.20%) - LeetSync

---
+++ 169-majority-element/majority-element.go
@@ -0,0 +1,16 @@
+func majorityElement(nums []int) int {
+    count := 0
+    candidate := 0
+    for _,num := range nums{
+        if count == 0{
+            candidate = num
+        }
+
+        if num == candidate{
+            count++
+        }else{
+            count--
+        }
+    }
+    return candidate
+}

4704431ab75bc45978af441509490332fe5fe12c
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-07-16 21:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (82.38%) - LeetSync

---
+++ 792-binary-search/binary-search.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+    return binarySearch(nums,0,len(nums)-1,target)
+}
+func binarySearch(nums[]int,low,high,target int) int{
+    if low > high {
+        return -1
+    }
+    mid := (low+high)/2
+    if nums[mid] == target{
+        return mid
+    }else if target > nums[mid]{
+        return binarySearch(nums,mid+1,high,target)
+    }else{
+        return binarySearch(nums,low,mid-1,target)
+    }
+}

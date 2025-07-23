194f26ee426612b96abb7ff0a10acbc9fb2e7a6c
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-07-23 21:18:29 +0530

    Time: 0 ms (100.00%) | Memory: 4.8 MB (34.57%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,17 +1,30 @@
 func searchInsert(nums []int, target int) int {
+    //Solution no 2
         left, right := 0, len(nums)-1
-
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {
-            return mid
-        } else if nums[mid] < target {
-            left = mid + 1
-        } else {
-            right = mid - 1
+        n := len(nums)
+        ans := n
+        for left <= right{
+            mid := (left+right)/2
+            if nums[mid] >= target{
+                ans = mid
+                right = mid - 1
+            }else{
+                left = mid + 1
+            }
         }
-    }
+        return ans
+    //Solution no 1
+    // for left <= right {
+    //     mid := left + (right-left)/2
+    //     if nums[mid] == target {
+    //         return mid
+    //     } else if nums[mid] < target {
+    //         left = mid + 1
+    //     } else {
+    //         right = mid - 1
+    //     }
+    // }
 
-    // target not found then left is inserted position
-    return left
+    // // target not found then left is inserted position
+    // return left
 }

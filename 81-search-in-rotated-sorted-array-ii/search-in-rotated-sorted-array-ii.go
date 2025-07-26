de6188329df5a199076a2872bef2152f51374673
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-07-26 21:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (75.66%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,32 @@
+func search(nums []int, target int) bool {
+    n := len(nums)
+   low, high := 0, n-1
+
+   for low <= high{
+    mid := (low+high)/2
+    if nums[mid] == target{
+        return true
+    }
+
+    if nums[low] == nums[mid] && nums[mid]==nums[high]{
+        low++
+        high--
+        continue
+    }
+
+    if nums[low] <= nums[mid]{
+        if nums[low] <= target && target <= nums[mid]{
+            high = mid-1
+        }else{
+            low = mid+1
+        }
+    }else{
+        if nums[mid] <= target && target <= nums[high]{
+            low = mid+1
+        }else{
+            high = mid - 1
+        }
+    }
+   } 
+    return false
+}

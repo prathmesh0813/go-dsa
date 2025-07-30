4029ea4267ef60650e44b12d8aa5b0ac02a571e4
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-07-30 22:12:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (33.84%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
@@ -1,7 +1,30 @@
 func singleNonDuplicate(nums []int) int {
-    xor := 0
-    for i := 0 ;i<len(nums);i++{
-        xor ^= nums[i]
+    n := len(nums)
+    if (n==1){
+        return nums[0]
     }
-    return xor
+
+    if nums[0] != nums[1] {
+        return nums[0]
+    }
+
+    if nums[n-1] != nums[n-2]{
+        return nums[n-1]
+    } 
+
+    low,high := 1, n-2
+    for low <= high{
+        mid := (low+high)/2
+        if nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]{
+            return nums[mid]
+        }
+
+        if (mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid %2 == 0 && nums[mid] == nums[mid+1]){
+            low = mid +1 
+        }else{
+            high = mid -1
+        }
+    }
+
+    return -1
 }

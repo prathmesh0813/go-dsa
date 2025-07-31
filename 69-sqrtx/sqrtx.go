3cdc227cc56e0fd1b0195b9c3308024b028702bc
prathmesh_gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-07-31 20:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (75.73%) - LeetSync

--- 69-sqrtx/sqrtx.go
+++ 69-sqrtx/sqrtx.go
@@ -1,14 +1,27 @@
 func mySqrt(x int) int {
-    if x == 0{
-        return 0
-    }
+//     if x == 0{
+//         return 0
+//     }
 
- r := x
-    for {
-        next := (r + x/r) / 2
-        if next >= r { 
-            return r
-        }
-        r = next
+//  r := x
+//     for {
+//         next := (r + x/r) / 2
+//         if next >= r { 
+//             return r
+//         }
+//         r = next
+//     }
+
+
+//Using binary search
+low,high := 1,x
+for low <= high{
+    mid := (low+high)/2
+    if (mid*mid) <= x{
+        low = mid+1
+    }else{
+        high = mid -1
     }
+}
+    return high
 }

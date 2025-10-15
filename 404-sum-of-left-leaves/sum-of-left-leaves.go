f84876601be974d6bc24060f225169375d81f871
Prathmesh Gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-10-15 20:48:10 +0530

    Time: 0 ms (100.00%) | Memory: 4.7 MB (49.13%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumOfLeftLeaves(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    sum := 0
+    if root.Left != nil {
+        if root.Left.Left == nil && root.Left.Right == nil{
+            sum += root.Left.Val
+        } else {
+            sum += sumOfLeftLeaves(root.Left)
+        }
+    }
+    sum += sumOfLeftLeaves(root.Right)
+    return sum
+}

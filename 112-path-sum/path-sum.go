da2e83da556e92163b8f24fb1b4b64328616fd5c
Prathmesh Gonjare <81070733+prathmesh0813@users.noreply.github.com>
2025-10-16 21:12:12 +0530

    Time: 3 ms (3.11%) | Memory: 6.7 MB (84.75%) - LeetSync

---
+++ 112-path-sum/path-sum.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    if root == nil {
+        return false
+    }
+
+    if root.Left == nil && root.Right == nil {
+        return root.Val == targetSum
+    }
+
+    remaining := targetSum - root.Val
+    return hasPathSum(root.Left,remaining) || hasPathSum(root.Right, remaining)
+}
